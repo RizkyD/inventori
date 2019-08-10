9444873190ebd93d2cd198d4a94e2b0a2655f539
RizkyD <dramadhan.rizky@gmail.com>
2019-08-10 15:34:34 +0700

    Logout + Modify Navbar

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -40,5 +41,9 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
+    }
+
+    protected function loggedOut(Request $request) {
+        return redirect('/');
     }
 }
